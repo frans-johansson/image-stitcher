c037a1969678d2df4aa7209f12e9562b4af09b79
Frans Johansson <fransjohansson98@gmail.com>
2021-10-05 09:52:27 +0200

    Updates to test code for showing correspondences
    Co-authored-by: Astrid Johansson <Astjo525@users.noreply.github.com>
    Co-authored-by: mater436 <mater436@users.noreply.github.com>

--- stitcher/__main__.py
+++ stitcher/__main__.py
@@ -3,7 +3,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import cv2
-import itertools
 from stitcher.images import ImageCollection
 from stitcher.features import FeatureHandler
 
@@ -39,48 +38,36 @@ if __name__ == "__main__":
     )
 
     # TEST: Display all the images
-    cv2.namedWindow("Test image collection")
+    # cv2.namedWindow("Test image collection")
 
-    for image in image_collection.low_res_images:
-        cv2.imshow("Test image collection", image)
-        cv2.waitKey(0)
+    # for image in image_collection.low_res_images:
+    #     cv2.imshow("Test image collection", image)
+    #     cv2.waitKey(0)
 
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
     # END TEST
 
     # Detect and match features
     feature_handler = FeatureHandler(image_collection.low_res_images)
 
     # TEST: Visualize some correspondences
-    matches = feature_handler.feature_matches
-    image_pairs = itertools.permutations(range(len(image_collection.low_res_images)), 2)
+    # matches = feature_handler.feature_matches
 
-    cv2.namedWindow("Test features")
+    # for i, i_matches in matches.items():
+    #     for j, ij_matches in i_matches.items():
+    #         ms = ij_matches[0].getMatches()
+    #         sorted(ms, key=lambda x: x.distance)
 
-    for pair in image_pairs:
-        ms = matches[pair][0].getMatches()
-        sorted(ms, key=lambda x: x.distance)
-        
-        kp1 = feature_handler.image_features[pair[0]].getKeypoints()
-        kp2 = feature_handler.image_features[pair[1]].getKeypoints()
-        img1 = image_collection.low_res_images[pair[0]]
-        img2 = image_collection.low_res_images[pair[1]]
+    #         kp1 = feature_handler.image_features[i].getKeypoints()
+    #         kp2 = feature_handler.image_features[j].getKeypoints()
+    #         img1 = image_collection.low_res_images[i]
+    #         img2 = image_collection.low_res_images[j]
 
-        print(f"Showing {len(ms)} matches")
+    #         print(f"Showing {len(ms)} matches")
 
-        match_img = cv2.drawMatches(img1, kp1, img2, kp2, ms, img2, flags=2)
-        cv2.imshow("Test features", match_img)
-        cv2.waitKey(0)
+    #         match_img = cv2.drawMatches(img1, kp1, img2, kp2, ms, img2, flags=2)
+    #         cv2.imshow(f"Test features, images: {(i, j)}", match_img)
+    #         cv2.waitKey(0)
 
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
     # END TEST
-
-    # ms = matches.getMatches()
-    # sorted(ms, key=lambda x: x.distance)
-    # kp1, kp2 = [f.getKeypoints() for f in features]
-
-    # match_img = cv2.drawMatches(img1, kp1, img2, kp2, ms[:20], img2, flags=2)
-    # cv2.imwrite("test.png", match_img)
-
-    
-
