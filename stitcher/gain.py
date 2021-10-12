a7078387608f4194f508af4741a59b98bcb4b39e
Matilda Eriksson <mater436@student.liu.se>
2021-10-12 11:41:11 +0200

    Added a working implemenation of gain compensation

---
+++ stitcher/gain.py
@@ -0,0 +1,125 @@
+"""Calculates and applies a gain to all images."""
+
+import numpy as np
+import itertools
+from scipy.optimize import least_squares
+
+class GainCompensator:
+    """
+    Takes a composite of images and calculates and applies the gain for each one.
+
+    Members:
+        projected_images: An array of images that has been perspective projected.
+        overlap_data: A dictionary holding the values of all overlapping pixels between the images.
+    """
+
+    def __init__(self, projected_images):
+        """
+        Initializes the gain compensator.
+
+        Args:
+            projected_images: A NxRxCx3 array, where N is the number of images, R is the number of rows in each image
+            and C is the number of columns in each image.
+        """
+        self.projected_images = projected_images
+        assert self.projected_images.ndim == 4, "Projected images have incorrect dimensions"
+        self.overlap_data = self._find_overlap()
+
+    def _find_overlap(self):
+        """
+        Finds where overlap occurs and updates the mask accordingly.
+
+        Returns:
+            mask: A RxC array with lists containting the indicies of all images having a valid pixel 
+        """
+        overlap_data = {}
+
+        for i in range(self.projected_images.shape[0]):
+            overlap_data[i] = {}
+
+        mask = np.all(self.projected_images >= 0, axis=3)
+
+        # Loop through all combinations of image pairs
+        for i, j in list(itertools.combinations(range(mask.shape[0]), 2)):
+                # Find indices where overlaps occur between the two images
+                temp_mask = np.logical_and(mask[i], mask[j])
+                idx = np.argwhere(temp_mask)
+
+                overlap_data[i][j] = []
+                overlap_data[j][i] = []
+
+                # Add indices of pixel and overlapping image to the dictionary
+                for r, c in idx:
+                    overlap_data[i][j].append(self.projected_images[i, r, c, :])
+                    overlap_data[j][i].append(self.projected_images[j, r, c, :])
+
+        self.mask = mask
+
+        return overlap_data
+
+
+    def _calculate_gain_error(self, g):
+        """
+        Calculate the gain errror.
+
+        Args:
+            g: A matrix of the approximated gain values.
+
+        Returns:
+            The calculated error value.
+        """
+        error = 0
+        sigma_n = 10
+        sigma_g = 0.1
+
+        for i, j in list(itertools.permutations(range(self.projected_images.shape[0]), 2)):
+            if len(self.overlap_data[i][j]) != 0:
+
+                mean_ij = np.mean(np.sum(self.overlap_data[i][j], axis=1)/3)
+                mean_ji = np.mean(np.sum(self.overlap_data[j][i], axis=1)/3)
+
+                error += len(self.overlap_data[i][j]) * ((g[i] * mean_ij - g[j] * mean_ji)**2 / sigma_n**2 + (1 - g[i])**2 / sigma_g**2)
+
+        return error * 0.5
+
+    def _calculate_gain(self):
+        """
+        Calculate the gain using a least squares solver.
+
+        Returns:
+            A matrix with the approxiamted gain values.
+        """
+        # Initialize the gain values to ones.
+        g = np.ones((self.projected_images.shape[0]))
+
+        # Run minimizer
+        res = least_squares(
+            self._calculate_gain_error,
+            g,
+            ftol=1e-3,
+            method="trf",
+        )
+
+        return res.x
+
+
+    def gain_compensate(self):
+        """
+        Perform the gain compensation by calculating the gain values, multiplying them with the images and
+        map them back to range 0-255, with values -1 where there are no pixel values.
+
+        Returns:
+            The gain compensated images, same size as self.projected_images.
+        """
+        # Calculate the gain values
+        g = self._calculate_gain()
+
+        # Multiply with the gain in pixels that do not have value -1, let those pixels keep the value -1
+        imgs = np.where(self.projected_images != -1, g[:, np.newaxis, np.newaxis, np.newaxis] * self.projected_images, -1)
+
+        # Rescale the values to be integers between 0-255, except for non-pixel values that are -1
+        imgs = (imgs / np.max(imgs)) * 255
+        imgs[imgs < 0] = - 1
+        imgs = np.floor(imgs)
+
+        return imgs
--- test_overlap.py
+++ test_overlap.py
@@ -1,32 +1,63 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from stitcher.overlap import OverlapMask
+from stitcher.gain import GainCompensator
+from pathlib import Path
+"""
+imgs = np.ones((2,100,100,3))*-1
 
-imgs = np.ones((5,100,100,3))*-1
+#imgs[0, :60, :60, :] = 0.2
+#imgs[1, 40:, 40:, :] = 0.8
+#imgs[2, 50:70, 50:70, :] = 0.5
+#imgs[3, 50:70, :20, :] = 0.9
+#imgs[4, 80:, :20, :] = 0.6
 
-imgs[0, :60, :60, :] = 0.2
-imgs[1, 40:, 40:, :] = 0.8
-imgs[2, 50:70, 50:70, :] = 0.5
-imgs[3, 50:70, :20, :] = 0.9
-imgs[4, 80:, :20, :] = 0.6 
-
-maskFinder = OverlapMask(imgs)
-
-mask = maskFinder.get_mask()
-
-print(mask.shape)
-
-list_overlap = maskFinder.get_overlap(3)
-
-print(list_overlap[5]) # Should say [50, 5, 0]
+imgs[0, :, :60, 0] = 200
+imgs[0, :, :60, 1] = 100
+imgs[0, :, :60, 2] = 56
+imgs[0, :, :20, 0] = 100
+imgs[0, :, :20, 1] = 60
+imgs[0, :, :20, 2] = 156
+imgs[1, :, 40:, 0] = 10
+imgs[1, :, 40:, 1] = 179
+imgs[1, :, 40:, 2] = 248
 
 """
-comb_img = np.zeros(imgs.shape[1:])
+images = [str(file) for file in Path("img/lunchroom").rglob("*.jpg")]
+scaling = 0.1
 
-print(comb_img.shape)
+imgs_1 = cv2.imread(images[0], cv2.IMREAD_COLOR)
+imgs_2 = cv2.imread(images[1], cv2.IMREAD_COLOR)
 
-comb_image = maskFinder.add_nonoverlap_to_image(comb_img)
-"""
-plt.imshow(mask)
+(height, width) = imgs_1.shape[:2]
+new_dims = (int(width * scaling), int(height * scaling))
+
+imgs_1 = cv2.resize(imgs_1, new_dims, interpolation=cv2.INTER_CUBIC)
+imgs_2 = cv2.resize(imgs_2, new_dims, interpolation=cv2.INTER_CUBIC)
+
+imgs = np.ones((2, imgs_1.shape[0], imgs_1.shape[1]+100, imgs_1.shape[2]))*-1
+
+imgs[0, :, :-100, :] = imgs_1
+imgs[1, :, 100:, :] = imgs_2
+
+print(imgs.shape)
+
+gain = GainCompensator(imgs)
+
+print("Starting gain compensation")
+
+images = gain.gain_compensate()
+
+imgs[imgs == -1] = 0
+images[images == -1] = 0
+
+plt.figure(1)
+plt.imshow((imgs[0]).astype('uint8'))
+plt.figure(2)
+plt.imshow((imgs[1]).astype('uint8'))
+plt.figure(3)
+plt.imshow((images[0]).astype('uint8'))
+plt.figure(4)
+plt.imshow((images[1]).astype('uint8'))
+
 plt.show()
